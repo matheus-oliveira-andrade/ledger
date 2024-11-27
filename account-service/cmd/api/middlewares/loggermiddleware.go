6a10ff3c86492b2ef63d57d761d6bfcc461fb7fd
Matheus de Oliveira de Andrade <mathnhais@outlook.com>
2024-11-27 20:17:04 -0300

    feat: add logger middleware

---
+++ account-service/cmd/api/middlewares/loggermiddleware.go
@@ -0,0 +1,22 @@
+package middlewares
+
+import (
+	"context"
+	"log/slog"
+	"net/http"
+
+	"github.com/matheus-oliveira-andrade/ledger/account-service/cmd/api/utils"
+	"github.com/matheus-oliveira-andrade/ledger/account-service/internal/logger"
+	"github.com/spf13/viper"
+)
+
+func UseLoggerMiddleware() func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			logger := logger.NewLogger(viper.GetString("SERVICE_NAME"), slog.LevelInfo, nil)
+			ctx := context.WithValue(r.Context(), utils.CtxLoggerKey, logger)
+
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+}
