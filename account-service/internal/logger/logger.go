2f702925c470746765a5c02c679c177aec1e8126
Matheus de Oliveira de Andrade <mathnhais@outlook.com>
2024-11-27 19:47:29 -0300

    feat: add logger abstraction

---
+++ account-service/internal/logger/logger.go
@@ -0,0 +1,46 @@
+package logger
+
+import (
+	"io"
+	"log/slog"
+	"os"
+)
+
+type LoggerInterface interface {
+	LogInformation(message string, args ...any)
+	LogWarning(message string, args ...any)
+	LogError(message string, args ...any)
+}
+
+type Logger struct {
+	slogger *slog.Logger
+}
+
+func NewLogger(serviceName string, minLevel slog.Level, output io.Writer) LoggerInterface {
+	if output == nil {
+		output = os.Stdout
+	}
+
+	jsonHandler := slog.NewJSONHandler(output, &slog.HandlerOptions{
+		Level:     minLevel,
+		AddSource: true,
+	})
+
+	logger := slog.New(jsonHandler).With(slog.String("service", serviceName))
+
+	return &Logger{
+		slogger: logger,
+	}
+}
+
+func (l *Logger) LogInformation(message string, args ...any) {
+	l.slogger.Info(message, args...)
+}
+
+func (l *Logger) LogWarning(message string, args ...any) {
+	l.slogger.Warn(message, args...)
+}
+
+func (l *Logger) LogError(message string, args ...any) {
+	l.slogger.Error(message, args...)
+}
