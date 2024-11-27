ce89e0459785112fff225bd5631c4462c49acde7
Matheus de Oliveira <32457879+matheus-oliveira-andrade@users.noreply.github.com>
2024-11-26 21:44:11 -0300

    feat(account): middleware request logger (#7)

---
+++ account-service/cmd/api/middlewares/requestsmiddleware.go
@@ -0,0 +1,42 @@
+package middlewares
+
+import (
+	"log/slog"
+	"net/http"
+	"time"
+)
+
+func RequestLoggerMiddleware() func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+			ww := &responseWriter{
+				ResponseWriter: w,
+				status:         http.StatusOK,
+			}
+
+			start := time.Now()
+			next.ServeHTTP(ww, r)
+			duration := time.Since(start)
+
+			slog.Info(
+				"Request received",
+				slog.String("method", r.Method),
+				slog.String("path", r.URL.Path),
+				slog.Int("status", ww.status),
+				slog.String("remote_addr", r.RemoteAddr),
+				slog.Duration("duration", duration),
+			)
+		})
+	}
+}
+
+type responseWriter struct {
+	http.ResponseWriter
+	status int
+}
+
+func (rw *responseWriter) WriteHeader(statusCode int) {
+	rw.status = statusCode
+	rw.ResponseWriter.WriteHeader(statusCode)
+}
