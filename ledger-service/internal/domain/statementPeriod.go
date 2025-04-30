316f7e988d519b6de84c0b932307e6dd801cf0e3
Matheus de Oliveira de Andrade <mathnhais@outlook.com>
2025-04-29 22:01:52 -0300

    feat(ledger): truncate time from period dates

--- ledger-service/internal/domain/statementPeriod.go
+++ ledger-service/internal/domain/statementPeriod.go
@@ -4,7 +4,11 @@ import "time"
 
 func GetStatementPeriodDates(period int) (time.Time, time.Time) {
 	startDate := time.Now().AddDate(0, 0, period*-1)
-	endDate := time.Now()
+	endDate := time.Now().AddDate(0, 0, 1)
 
-	return startDate, endDate
+	return truncateTime(startDate), truncateTime(endDate)
+}
+
+func truncateTime(t time.Time) time.Time {
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
 }
